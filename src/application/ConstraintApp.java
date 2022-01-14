d008fe42ce89da027ce6bb3b2bb2fd8ea527ee0c
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-01-15 05:33:43 +0700

    add Bounded Type Parameter (contravariant)

---
+++ src/application/ConstraintApp.java
@@ -0,0 +1,29 @@
+package application;
+
+public class ConstraintApp {
+    public static void main(String[] args) {
+
+        NumberData<Integer> integerNumberData = new NumberData<>(23);
+        NumberData<Long> longNumberData = new NumberData<>(1000L);
+
+        // Cannot using String (class NumberData can only use child from Number)
+        // NumberData<String> stringNumberData = new NumberData<String>("Xenosty");
+    }
+
+    // Only child from Number can be used (Integer, Float, Double...)
+    public static class NumberData<T extends Number> {
+        private T data;
+
+        public NumberData(T data) {
+            this.data = data;
+        }
+
+        public T getData() {
+            return data;
+        }
+
+        public void setData(T data) {
+            this.data = data;
+        }
+    }
+}
